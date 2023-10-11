9c8eb929ceed0b88697a433d724e077ff8fa6939
Jan Höck <jan.hoeck@deepl.com>
2023-10-11 18:08:55 +0200

    Fix build ?

--- next.config.js
+++ next.config.js
@@ -4,7 +4,22 @@ const isProduction = process.env.NODE_ENV === "production";
 const nextConfig = {
     reactStrictMode: true,
     basePath: isProduction ? '/jan_hoeck' : '',
-    assetPrefix: isProduction ? '/jan_hoeck' : ''
+    assetPrefix: isProduction ? '/jan_hoeck' : '',
+    /**
+     * Enable static exports for the App Router.
+     *
+     * @see https://nextjs.org/docs/pages/building-your-application/deploying/static-exports
+     */
+    output: "export",
+    /**
+     * Disable server-based image optimization. Next.js does not support
+     * dynamic features with static exports.
+     *
+     * @see https://nextjs.org/docs/pages/api-reference/components/image#unoptimized
+     */
+    images: {
+        unoptimized: true,
+    },
 }
 
 module.exports = nextConfig
