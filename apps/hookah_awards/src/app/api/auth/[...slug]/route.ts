246d090ec6e82373a0df3bb5c3c242c070c395bc
Jan <jan.hoeck@gmx.net>
2025-11-12 19:50:50 +0100

    Debugging env variables (5)

--- apps/hookah_awards/src/app/api/auth/[...slug]/route.ts
+++ apps/hookah_awards/src/app/api/auth/[...slug]/route.ts
@@ -1,16 +1,4 @@
+import { auth } from '@/lib/auth'
 import { toNextJsHandler } from 'better-auth/next-js'
 
-async function getHandlers() {
-  const { auth } = await import('../../../../lib/auth')
-  return toNextJsHandler(auth)
-}
-
-export const GET = async (request: Request) => {
-  const handlers = await getHandlers()
-  return handlers.GET(request)
-}
-
-export const POST = async (request: Request) => {
-  const handlers = await getHandlers()
-  return handlers.POST(request)
-}
+export const { POST, GET } = toNextJsHandler(auth)
