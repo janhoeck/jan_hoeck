d85bff00a4b74fa93cb112547c5079369020f529
Jan <jan.hoeck@gmx.net>
2025-11-12 20:19:06 +0100

    Fix (2)

--- apps/hookah_awards/src/app/page.tsx
+++ apps/hookah_awards/src/app/page.tsx
@@ -2,7 +2,5 @@ import { HomeView } from '@/components/views/home/HomeView'
 import { loadData } from '@/lib/load-data'
 
 export default function HomePage() {
-  const categoriesWithVideos = loadData()
-
-  return <HomeView categoriesWithVideos={categoriesWithVideos} />
+  return <div>Das ist ein Test</div>
 }
