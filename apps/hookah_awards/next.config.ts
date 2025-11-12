bd5a1f6717a6cb196f52cc80937a30f891d308d9
Jan <jan.hoeck@gmx.net>
2025-11-12 20:07:22 +0100

    Change output folder (2)

--- apps/hookah_awards/next.config.ts
+++ apps/hookah_awards/next.config.ts
@@ -1,16 +1,27 @@
 import { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  distDir: 'out',
   reactStrictMode: true,
   basePath: '',
   images: {
     remotePatterns: [
-      new URL('https://img.youtube.com/vi/**'),
-      new URL('https://i.ytimg.com/vi/**'),
-      new URL('https://static-cdn.jtvnw.net/user-default-pictures-uv/**'),
+      {
+        protocol: 'https',
+        hostname: 'img.youtube.com',
+        pathname: '/vi/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.ytimg.com',
+        pathname: '/vi/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'static-cdn.jtvnw.net',
+        pathname: '/user-default-pictures-uv/**',
+      },
     ],
   },
 }
 
-module.exports = nextConfig
+export default nextConfig
