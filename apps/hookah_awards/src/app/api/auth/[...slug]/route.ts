e6505e8c72f889b068900112c8a170f47b2b1881
Jan <jan.hoeck@gmx.net>
2025-11-12 19:29:42 +0100

    Debugging env variables (2)

--- apps/hookah_awards/src/app/api/auth/[...slug]/route.ts
+++ apps/hookah_awards/src/app/api/auth/[...slug]/route.ts
@@ -1,5 +1,18 @@
+'use server'
+
 import { toNextJsHandler } from 'better-auth/next-js'
 
-import { auth } from '../../../../lib/auth'
+async function getHandlers() {
+  const { auth } = await import('../../../../lib/auth')
+  return toNextJsHandler(auth)
+}
 
-export const { POST, GET } = toNextJsHandler(auth)
+export const GET = async (request: Request) => {
+  const handlers = await getHandlers()
+  return handlers.GET(request)
+}
+
+export const POST = async (request: Request) => {
+  const handlers = await getHandlers()
+  return handlers.POST(request)
+}
--- apps/hookah_awards/src/lib/auth.ts
+++ apps/hookah_awards/src/lib/auth.ts
@@ -1,12 +1,12 @@
 import { betterAuth } from 'better-auth'
 
-console.log('auth-url', process.env.BETTER_AUTH_URL)
+console.log('auth-url', process.env.NEXT_PUBLIC_BETTER_AUTH_URL)
 console.log('twitch-client-id', process.env.TWITCH_CLIENT_ID)
 console.log('twitch-client-secret', process.env.TWITCH_CLIENT_SECRET)
 console.log('secret', process.env.BETTER_AUTH_SECRET)
 
 export const auth = betterAuth({
-  baseURL: process.env.BETTER_AUTH_URL! as string,
+  baseURL: process.env.NEXT_PUBLIC_BETTER_AUTH_URL! as string,
   emailAndPassword: {
     enabled: false,
   },
