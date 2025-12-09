6bfe06185972d3c9a9581326844722fad12d738a
Jan Höck <jan.hoeck@deepl.com>
2025-12-09 10:51:27 +0100

    Add imprint to hookah awards

--- apps/hookah_awards/src/components/views/imprint/ImprintView.tsx
+++ apps/hookah_awards/src/components/views/imprint/ImprintView.tsx
@@ -2,9 +2,26 @@ import { Headline } from '@/components/shared/Headline'
 
 export const ImprintView = () => {
   return (
-    <div className='container mx-auto max-w-6xl'>
+    <div className='container mx-auto max-w-6xl text-muted-foreground'>
       <Headline>Impressum</Headline>
-      <p>In arbeit...</p>
+      <div className='space-y-2'>
+        <div>Aykut_Hookah - Aykut Sakarkaya</div>
+        <div>c/o Online-Impressum.de #5231</div>
+        <div>Europaring 90 53757</div>
+        <div>Sankt Augustin</div>
+      </div>
+      <br />
+      <div>E-Mail: aykut-hookah@mail.online-impressum.de</div>
+      <br />
+      <div className='space-y-2'>
+        <div>Zuständige Regulierungs- und Aufsichtsbehörde:</div>
+        <div>Medienanstalt Rheinland-Pfalz</div>
+        <div>Sitz: Deutschland</div>
+      </div>
+      <br />
+      <div className='text-muted-foreground'>
+        Kein Umsatzsteuerausweis aufgrund Anwendung der Kleinunternehmerregelung gemäß § 19 UStG.
+      </div>
     </div>
   )
 }
